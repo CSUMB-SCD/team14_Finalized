baf3ba3e317825817187ebc3599337cf5c94cf25
resteybar <resteybar@csumb.edu>
2018-11-27 20:00:31 -0800

    Can view input of username & password

--- ng6/src/app/signin/signin.component.ts
+++ ng6/src/app/signin/signin.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+
+  }
+
+  loginUser(event) {
+    event.preventDefault();
+    const target = event.target;
+    const passed_in_un = target.querySelector('#username').value;
+    const passed_in_pw = target.querySelector('#password').value;
+
+    console.log(passed_in_un, passed_in_pw);
+  }
 }
