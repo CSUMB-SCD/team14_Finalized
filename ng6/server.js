c5fde37e71cf1dbed7f5a6fb8807741a425c9614
Marco Aguilera <maaguilera@csumb.edu>
2018-12-04 11:33:59 -0800

    Heroku Test Fix

--- ng6/server.js
+++ ng6/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/ng6'));
 
 app.listen(process.env.PORT || 8080);
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', () =>{
+  console.log('Server started');
 })
 
-console.log('Console listening!');
 
